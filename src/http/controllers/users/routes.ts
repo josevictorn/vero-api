c1c9c19376a2e975d1bcf9f49c5df36da3a7faef
José Victor <josevictornascimento2016@gmail.com>
2026-04-11 12:51:51 -0300

    feat: :sparkles: implement ProfileController for retrieving authenticated user profile

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,10 @@
 import { RegisterUserController } from "@controllers/users/register";
 import type { FastifyInstance } from "fastify";
 import { AuthenticateController } from "./authenticate";
+import { ProfileController } from "./profile";
 
 export async function usersRoutes(app: FastifyInstance) {
 	await app.register(RegisterUserController);
 	await app.register(AuthenticateController);
+	await app.register(ProfileController);
 }
