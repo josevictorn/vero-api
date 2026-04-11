3048dd51ae1efd5984e6e7fabce62650c3560001
José Victor <josevictornascimento2016@gmail.com>
2026-04-11 12:32:00 -0300

    feat: :sparkles: implement AuthenticateController for user authentication

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,6 +1,8 @@
 import { RegisterUserController } from "@controllers/users/register";
 import type { FastifyInstance } from "fastify";
+import { AuthenticateController } from "./authenticate";
 
 export async function usersRoutes(app: FastifyInstance) {
 	await app.register(RegisterUserController);
+	await app.register(AuthenticateController);
 }
