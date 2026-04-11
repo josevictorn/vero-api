33949894f33064d34f4d836b2ed2c78d20edd511
José Victor <josevictornascimento2016@gmail.com>
2026-04-11 13:49:35 -0300

    feat: :sparkles: implement FetchUsersController for paginated user retrieval and add factory function for use case

---
+++ src/use-cases/users/factories/make-fetch-users-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { FetchUsersUseCase } from "../fetch-users";
+
+export function makeFetchUsersUseCase() {
+	const usersRepository = new PrismaUsersRepository();
+	const fetchUsersUseCase = new FetchUsersUseCase(usersRepository);
+
+	return fetchUsersUseCase;
+}
