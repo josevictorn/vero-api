506d80fa1c7af6fb7de8029716a2344f599034ea
André Lira <andrelira50@gmail.com>
2026-04-17 18:54:41 -0300

    feat: add lawyers repository

---
+++ src/repositories/prisma/prisma-lawyers-repository.ts
@@ -0,0 +1,64 @@
+import type { Lawyer, Prisma } from "@generated/prisma/client";
+import type { LawyersRepository } from "@repositories/lawyers-repository";
+import { prisma } from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/utils/constants";
+import type { PaginationParams } from "@/utils/pagination-params";
+
+export class PrismaLawyersRepository implements LawyersRepository {
+	async findById(id: string) {
+		const lawyer = await prisma.lawyer.findUnique({
+			where: { id },
+		});
+
+		return lawyer;
+	}
+
+	async findByUserId(userId: string) {
+		const lawyer = await prisma.lawyer.findUnique({
+			where: { userId },
+		});
+
+		return lawyer;
+	}
+
+	async create(data: Prisma.LawyerUncheckedCreateInput) {
+		const lawyer = await prisma.lawyer.create({
+			data,
+		});
+
+		return lawyer;
+	}
+
+	async findMany(params: PaginationParams) {
+		const [lawyers, total] = await prisma.$transaction([
+			prisma.lawyer.findMany({
+				skip: (params.page - 1) * ITEM_PER_PAGE,
+				take: ITEM_PER_PAGE,
+				orderBy: {
+					createdAt: "desc",
+				},
+			}),
+			prisma.lawyer.count(),
+		]);
+
+		return {
+			items: lawyers,
+			total,
+		};
+	}
+
+	async save(data: Lawyer) {
+		const updatedLawyer = await prisma.lawyer.update({
+			where: { id: data.id },
+			data,
+		});
+
+		return updatedLawyer;
+	}
+
+	async delete(id: string) {
+		await prisma.lawyer.delete({
+			where: { id },
+		});
+	}
+}
