d155947bb9edbe55601b175563205045b65cc8f9
José Victor <josevictornascimento2016@gmail.com>
2026-05-02 18:54:54 -0300

    feat: add additional fields to FetchClients and GetClient controllers

--- src/http/controllers/clients/get.ts
+++ src/http/controllers/clients/get.ts
@@ -22,50 +22,72 @@ export const GetClientController: FastifyPluginAsyncZod = async (app) => {
 						name: z.string(),
 						email: z.string(),
 						cellphone: z.string(),
+						maritalStatus: z.string(),
+						profession: z.string(),
+						rg: z.string(),
+						issuingAgency: z.string(),
+						cpf: z.string(),
+						street: z.string(),
+						neighborhood: z.string(),
+						city: z.string(),
+						state: z.string(),
+						zipCode: z.string(),
 						workspaceId: z.uuid(),
 						lawyerId: z.uuid().nullable(),
 						createdAt: z.date(),
 						updatedAt: z.date(),
 					}),
-                    400: z.object({ message: z.string() }).describe("Invalid request."),
+					400: z.object({ message: z.string() }).describe("Invalid request."),
 					404: z.object({ message: z.string().describe("Client not found.") }),
-                    500: z.object({ message: z.string() }).describe(("Internal server error.")),
+					500: z
+						.object({ message: z.string() })
+						.describe("Internal server error."),
 				},
 			},
 		},
 		async (request, reply) => {
-            const { id } = request.params;
-            const getClientUseCase = makeGetClientUseCase();
+			const { id } = request.params;
+			const getClientUseCase = makeGetClientUseCase();
 
-            const result = await getClientUseCase.execute({ clientId: id });
+			const result = await getClientUseCase.execute({ clientId: id });
 
-            if (result.isLeft()) {
-                const error = result.value;
+			if (result.isLeft()) {
+				const error = result.value;
 
-                switch (error.constructor) {
-                    case ClientNotFoundError:
+				switch (error.constructor) {
+					case ClientNotFoundError:
 						return reply.status(HTTP_STATUS.NOT_FOUND).send({
 							message: error.message,
 						});
-                    default:
+					default:
 						return reply.status(HTTP_STATUS.BAD_REQUEST).send({
 							message: "An unexpected error occurred.",
 						});
-                }
-            }
+				}
+			}
 
-            const { client } = result.value;
+			const { client } = result.value;
 
-            return reply.status(HTTP_STATUS.OK).send({
-                id: client.id,
-                name: client.name,
-                email: client.email,
-                cellphone: client.cellphone,
-                workspaceId: client.workspaceId,
-                lawyerId: client.lawyerId,
-                createdAt: client.createdAt,
-                updatedAt: client.updatedAt,
-            })
+			return reply.status(HTTP_STATUS.OK).send({
+				id: client.id,
+				name: client.name,
+				email: client.email,
+				cellphone: client.cellphone,
+				workspaceId: client.workspaceId,
+				lawyerId: client.lawyerId,
+				createdAt: client.createdAt,
+				updatedAt: client.updatedAt,
+				maritalStatus: client.maritalStatus,
+				profession: client.profession,
+				rg: client.rg,
+				issuingAgency: client.issuingAgency,
+				cpf: client.cpf,
+				street: client.street,
+				neighborhood: client.neighborhood,
+				city: client.city,
+				state: client.state,
+				zipCode: client.zipCode,
+			});
 		}
 	);
 };
